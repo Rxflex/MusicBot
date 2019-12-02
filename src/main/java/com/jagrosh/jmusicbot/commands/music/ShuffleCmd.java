690ab4ad74f591927c16155643da7c74795176b7
Rxflex <39628283+Rxflex@users.noreply.github.com>
2019-12-02 15:40:15 +0200

    Update ShuffleCmd.java

--- src/main/java/com/jagrosh/jmusicbot/commands/music/ShuffleCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/music/ShuffleCmd.java
@@ -30,7 +30,7 @@ public class ShuffleCmd extends MusicCommand
     {
         super(bot);
         this.name = "shuffle";
-        this.help = "shuffles songs you have added";
+        this.help = "перемешивает трэки которые вы добавили";
         this.aliases = bot.getConfig().getAliases(this.name);
         this.beListening = true;
         this.bePlaying = true;
@@ -44,13 +44,13 @@ public class ShuffleCmd extends MusicCommand
         switch (s) 
         {
             case 0:
-                event.replyError("You don't have any music in the queue to shuffle!");
+                event.replyError("У вас нет музыки в очереди для перемешивания");
                 break;
             case 1:
-                event.replyWarning("You only have one song in the queue!");
+                event.replyWarning("У вас только один трек в очереди!");
                 break;
             default:
-                event.replySuccess("You successfully shuffled your "+s+" entries.");
+                event.replySuccess("Вы успешно перемешали "+s+" трэков в очереди.");
                 break;
         }
     }
