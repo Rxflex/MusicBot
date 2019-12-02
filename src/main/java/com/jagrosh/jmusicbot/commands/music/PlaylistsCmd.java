fccf32c0909f5e8d058bc808bcc569708f920579
Rxflex <39628283+Rxflex@users.noreply.github.com>
2019-12-02 15:26:18 +0200

    Update PlaylistsCmd.java

--- src/main/java/com/jagrosh/jmusicbot/commands/music/PlaylistsCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/music/PlaylistsCmd.java
@@ -44,19 +44,19 @@ public class PlaylistsCmd extends MusicCommand
             bot.getPlaylistLoader().createFolder();
         if(!bot.getPlaylistLoader().folderExists())
         {
-            event.reply(event.getClient().getWarning()+" Playlists folder does not exist and could not be created!");
+            event.reply(event.getClient().getWarning()+" Папка с плейлистами не найдена и не может быть создана!");
             return;
         }
         List<String> list = bot.getPlaylistLoader().getPlaylistNames();
         if(list==null)
-            event.reply(event.getClient().getError()+" Failed to load available playlists!");
+            event.reply(event.getClient().getError()+" Ошибка загрузки возможных плейлистов!");
         else if(list.isEmpty())
-            event.reply(event.getClient().getWarning()+" There are no playlists in the Playlists folder!");
+            event.reply(event.getClient().getWarning()+" Плейлисты в БД не обнаружены!");
         else
         {
-            StringBuilder builder = new StringBuilder(event.getClient().getSuccess()+" Available playlists:\n");
+            StringBuilder builder = new StringBuilder(event.getClient().getSuccess()+" Доступные плейлисты:\n");
             list.forEach(str -> builder.append("`").append(str).append("` "));
-            builder.append("\nType `").append(event.getClient().getTextualPrefix()).append("play playlist <name>` to play a playlist");
+            builder.append("\nType `").append(event.getClient().getTextualPrefix()).append("play playlist <name>` для воспроизведения плейлиста");
             event.reply(builder.toString());
         }
     }
