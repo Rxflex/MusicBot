d3386ce5865ce91fc9bf96c13a598a84534c84af
Rxflex <39628283+Rxflex@users.noreply.github.com>
2019-12-02 15:15:26 +0200

    Update NowplayingCmd.java

--- src/main/java/com/jagrosh/jmusicbot/commands/music/NowplayingCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/music/NowplayingCmd.java
@@ -32,7 +32,7 @@ public class NowplayingCmd extends MusicCommand
     {
         super(bot);
         this.name = "nowplaying";
-        this.help = "shows the song that is currently playing";
+        this.help = "отображает какой трэк сейчас играет";
         this.aliases = bot.getConfig().getAliases(this.name);
         this.botPermissions = new Permission[]{Permission.MESSAGE_EMBED_LINKS};
     }
